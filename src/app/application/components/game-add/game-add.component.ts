eb02a23b26575a9fd90eb5b5f7bf367d71ddd79f
nourhene18 <nourhenebenslema@gmail.com>
2024-11-13 20:59:22 +0100

    Partie 2 Application TP8 finie

--- src/app/application/components/game-add/game-add.component.ts
+++ src/app/application/components/game-add/game-add.component.ts
@@ -1,14 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Category } from '../../model/category';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { GameService } from '../../services/game.service';
+import { Game } from '../../model/game';
 
 
 @Component({
   selector: 'app-game-add',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
+  providers:[GameService],
   templateUrl: './game-add.component.html',
   styleUrl: './game-add.component.css'
 })
 export class GameAddComponent  {
   categories = Object.values(Category);
+  games: Game[]=[];
+  gameForm:FormGroup=new FormGroup({
+    id:new FormControl(1, {nonNullable:true}),
+    name:new FormControl("Echec", {nonNullable:true}),
+    price:new FormControl(46.3, {nonNullable:true}),
+    madeIn:new FormControl('Tunisie', {nonNullable:true}),
+    category:new FormControl(Category.BoardGames, {nonNullable:true}),
+    isNew:new FormControl(true, {nonNullable:true}),
+
+  })
+  gameService: GameService=inject(GameService);
+
+  ngOnInit(): void {
+    this.gameService.getGames().subscribe((games) => {
+      this.games = games;
+    });
+  }
+  onSubmit(): void {
+    let newId = this.games.length+1;
+    this.gameForm.get('id')?.setValue(newId);
+
+    this.gameService.addGame(this.gameForm.value).subscribe((game) => {
+      this.games.push(game);  
+      console.log('Jeu ajouté:', game);
+    });
+  }
+
+  onResetForm(): void {
+    this.gameForm.reset();
+    this.gameForm.get('id')?.setValue(this.games.length+1);
+
+  }
 }
--- src/app/application/services/game.service.ts
+++ src/app/application/services/game.service.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Game } from '../model/game';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
+  private apiUrl = 'http://localhost:3000/games';
 
+  http: HttpClient=inject(HttpClient);
+
+  getGames(): Observable<Game[]> {
+    return this.http.get<Game[]>(this.apiUrl);
+  }
+  addGame(game: Game): Observable<Game> {
+    return this.http.post<Game>(this.apiUrl, game);
+  }
 }
